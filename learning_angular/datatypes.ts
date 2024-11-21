7b6d815a4b16d8a559c69854328eab3f2676646a
Aiert <36206587+Otxobot@users.noreply.github.com>
2024-11-21 11:08:16 +0000

    different function types in typescript

--- learning_angular/datatypes.ts
+++ learning_angular/datatypes.ts
@@ -27,6 +27,8 @@ let numList: Array<number>;
 
 numList = [1,2,3,4,5];
 
+let newNum = numList[4];
+
 let results = numList.filter((num)=> num > 2);
 
 let emp = empList.find((emp)=> emp === "hello");
@@ -36,3 +38,48 @@ let sum = numList.reduce((acc, num) => acc + num);
 console.log(results);
 console.log(emp);
 console.log(sum);
+
+console.log("------Enums: ---------")
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let a: Color = Color.Red;
+let b: Color = Color.Green;
+let c: Color = Color.Blue;
+
+console.log("Red: " + a);
+console.log("Green: " + b);
+console.log("Blue: " + c);
+
+console.log("------Tuples: ---------")
+
+let swapNumbs: [number,number];
+
+swapNumbs = [10, 20];
+
+function swapNumbers (num1:number, num2:number) : [number, number] {
+    return [num2, num1]
+}
+
+console.log("Before: ")
+console.log(swapNumbs[0]);
+console.log(swapNumbs[1]);
+
+swapNumbs = swapNumbers(10, 20);
+
+console.log("After: ")
+console.log(swapNumbs[0]);
+console.log(swapNumbs[1]);
+
+console.log("------Any: ---------");
+
+let department;
+
+//I can set any to any data type, try to avoid any, always use the correct data type
+
+department = "IT";
+department = 10;
